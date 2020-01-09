e2e4d048b42d09e4bba8027e3483d6382ce98265
Alex Baranowski <alex@euro-linux.com>
2020-01-09 21:05:22 +0100

    els-ASAP auto commit file/tag gnome-applets-2.28.0-7.el6.src.rpm

---
+++ src/gweather/gweather.h
@@ -0,0 +1,55 @@
+#ifndef __GWEATHER_H__
+#define __GWEATHER_H__
+
+/*
+ *  todd kulesza <fflewddur@dropline.net>
+ *
+ *  This code released under the GNU GPL.
+ *  Read the file COPYING for more information.
+ *
+ * main header file
+ *
+ */
+
+#include <panel-applet.h>
+
+#include <libgweather/gweather-gconf.h>
+#include <libgweather/gweather-prefs.h>
+
+
+/* Radar map on by default. */
+#define RADARMAP
+
+G_BEGIN_DECLS
+ 
+typedef struct _GWeatherApplet GWeatherApplet;
+
+struct _GWeatherApplet
+{
+	PanelApplet *applet;
+	WeatherInfo *gweather_info;
+
+	GWeatherGConf *gconf;
+
+	GtkWidget *container;
+	GtkWidget *box;	
+	GtkWidget *label;
+	GtkWidget *image;
+	
+	PanelAppletOrient orient;
+	gint size;
+	gint timeout_tag;
+	gint suncalc_timeout_tag;
+
+	/* preferences  */
+	GWeatherPrefs gweather_pref;
+
+	GtkWidget *pref_dialog;
+
+	/* dialog stuff */
+	GtkWidget *details_dialog;
+};
+
+G_END_DECLS
+ 
+#endif
---
+++ src/gweather/main.c
@@ -0,0 +1,70 @@
+/* $Id$ */
+
+/*
+ *  Papadimitriou Spiros <spapadim+@cs.cmu.edu>
+ *
+ *  This code released under the GNU GPL.
+ *  Read the file COPYING for more information.
+ *
+ *  Main applet widget
+ *
+ */
+
+#include <glib.h>
+#include <config.h>
+#include <gtk/gtk.h>
+#include <panel-applet.h>
+
+#define GWEATHER_I_KNOW_THIS_IS_UNSTABLE
+
+#include <libgweather/gweather-gconf.h>
+#include <libgweather/gweather-prefs.h>
+
+#include "gweather.h"
+#include "gweather-pref.h"
+#include "gweather-dialog.h"
+#include "gweather-applet.h"
+
+
+static gboolean
+gweather_applet_new(PanelApplet *applet, const gchar *iid, gpointer data)
+{
+	GWeatherApplet *gw_applet;
+
+	char *prefs_key = panel_applet_get_preferences_key(applet);
+
+	panel_applet_add_preferences(applet, "/schemas/apps/gweather/prefs", NULL);
+	
+	gw_applet = g_new0(GWeatherApplet, 1);   
+	
+	gw_applet->applet = applet;
+	gw_applet->gweather_info = NULL;
+	gw_applet->gconf = gweather_gconf_new(prefs_key);
+	g_free (prefs_key);
+    	gweather_applet_create(gw_applet);
+
+    	gweather_prefs_load(&gw_applet->gweather_pref, gw_applet->gconf);
+    
+    	gweather_update(gw_applet);
+
+    	return TRUE;
+}
+
+static gboolean
+gweather_applet_factory(PanelApplet *applet,
+							const gchar *iid,
+							gpointer data)
+{
+	gboolean retval = FALSE;
+	
+	retval = gweather_applet_new(applet, iid, data);
+	
+	return retval;
+}
+
+PANEL_APPLET_BONOBO_FACTORY("OAFIID:GNOME_GWeatherApplet_Factory",
+			PANEL_TYPE_APPLET,
+			"gweather",
+			"0",
+			gweather_applet_factory,
+			NULL);
